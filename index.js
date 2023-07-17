35fccefb9776c0ff14e926681fa238fab1bda73e
unknown <meherabm803@gmail.com>
2023-07-17 23:25:28 +0600

    edit coment

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
-// middlewear
+
 app.use(cors());
 app.use(express.json());
 
